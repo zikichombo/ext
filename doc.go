166f40d6d8e20e67ec2710b7e2a36846dbdbe1cd
Scott <wsc@iri-labs.com>
2018-09-05 00:37:41 +0200

    add licensing and ext doc.go

---
+++ doc.go
@@ -0,0 +1,12 @@
+// Copyright 2018 The ZikiChomgo Authors. All rights reserved.  Use of this source
+// code is governed by a license that can be found in the License file.
+
+// Package ext houses zc collaboration with external imports.
+//
+// Package ext provides a place for collaboration outside
+// the licensing constraints of zikichombo.org (those constraints
+// being imports only zikichombo.org + go standard library + OS host code).
+//
+// Consumers need to verify licensing transitively of all imports in package
+// ext.
+package ext /* import zikichombo.org/ext */
