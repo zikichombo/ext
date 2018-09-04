79a297e564a221693a5a17f49009ae7d6f28a545
Scott <wsc@iri-labs.com>
2018-09-05 00:39:13 +0200

    licensing cleanup

--- flac/decoder.go
+++ flac/decoder.go
@@ -1,3 +1,6 @@
+// Copyright 2018 The ZikiChomgo Authors. All rights reserved.  Use of this source
+// code is governed by a license that can be found in the License file.
+
 // TODO: add seek support; only enabled in the dev branch of mewkiz/flac.
 
 // Note, ZikiChombo uses the terminology frame to refer to a single sample per
--- flac/doc.go
+++ flac/doc.go
@@ -1,2 +1,5 @@
+// Copyright 2018 The ZikiChomgo Authors. All rights reserved.  Use of this source
+// code is governed by a license that can be found in the License file.
+
 // Package flac provides decoding of FLAC audio streams.
 package flac /* import "zikichombo.org/ext/flac" */
