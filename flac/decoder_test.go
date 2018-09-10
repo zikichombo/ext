11673c30c1ece8169e81ae602833ba04d6aed191
Scott <wsc@iri-labs.com>
2018-09-10 18:27:25 +0200

    a little test for registering new codec with flac

--- flac/decoder_test.go
+++ flac/decoder_test.go
@@ -3,7 +3,31 @@
 
 package flac
 
-import "zikichombo.org/sound"
+import (
+	"os"
+	"testing"
+
+	"zikichombo.org/codec"
+	"zikichombo.org/sio"
+	"zikichombo.org/sound"
+)
 
 // Assert that flac.Decoder implements the sound.Source interface.
 var _ sound.Source = (*Decoder)(nil)
+
+func TestRegister(t *testing.T) {
+	co, err := codec.CodecFor(".flac", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	f, err := os.Open("0.flac")
+	if err != nil {
+		t.Fatal(err)
+	}
+	dec, _, err := co.Decoder(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer dec.Close()
+	sio.Play(dec)
+}
---
+++ flac/register.go
@@ -0,0 +1,26 @@
+package flac
+
+import (
+	"io"
+
+	"zikichombo.org/codec"
+	"zikichombo.org/sound"
+	"zikichombo.org/sound/sample"
+)
+
+type flacCodec struct {
+	codec.NullCodec
+}
+
+func (f *flacCodec) Extensions() []string {
+	return []string{".flc", ".flac"}
+}
+
+func (f *flacCodec) Decoder(rc io.ReadCloser) (sound.Source, sample.Codec, error) {
+	dec, err := NewDecoder(rc)
+	return dec, codec.AnySampleCodec, err
+}
+
+func init() {
+	codec.RegisterCodec(&flacCodec{})
+}
