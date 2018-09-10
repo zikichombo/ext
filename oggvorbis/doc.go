b7f7e6b4957684e9d7783db0a63c1ce1a228cb51
Scott <wsc@iri-labs.com>
2018-09-10 23:14:51 +0200

    license header+author, vanity url

---
+++ oggvorbis/doc.go
@@ -0,0 +1,4 @@
+// Copyright 2018 The ZikiChombo Authors. All rights reserved.  Use of this source
+// code is governed by a license that can be found in the License file.
+
+package oggvorbis /* import "zikichombo.org/ext/oggvorbis" */
